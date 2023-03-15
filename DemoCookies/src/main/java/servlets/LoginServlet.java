e6fbf951e17bc73a50b4a9c75bcc26f3628d17f9
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-14 20:32:49 -0700

    Add cookie functionality

--- DemoCookies/src/main/java/servlets/LoginServlet.java
+++ DemoCookies/src/main/java/servlets/LoginServlet.java
@@ -1,8 +1,11 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String COOKIE_NAME = "user-id";
+	private static final HashMap<String, String> userMap =new HashMap<>();;
      
     public LoginServlet() {
         super();
@@ -18,9 +23,33 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		response.getWriter().println("Login details received...");
-		response.getWriter().println("email: " + email);
-		response.getWriter().println("password: " + "*".repeat(password.length()));
+		//response.getWriter().println("Login details received...");
+		//response.getWriter().println("email: " + email);
+		//response.getWriter().println("password: " + "*".repeat(password.length()));
+		Cookie userCookie = new Cookie(COOKIE_NAME, email);
+		userMap.put(email, password);
+		response.addCookie(userCookie);
+		response.getWriter().println(email + " logged in !!!");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Cookie[] cookies = req.getCookies();
+		Cookie userCookie = null;
+		for(Cookie cookie: cookies) {
+			if(cookie.getName().equals(COOKIE_NAME)) {
+				userCookie = cookie;
+				break;
+			}
+		}
+		
+		if(null == userCookie) {
+			resp.getWriter().println("No user logged in");
+			return;
+		}
+		resp.getWriter().println("Login details received...");
+		resp.getWriter().println("email: " + userCookie.getValue());
+		resp.getWriter().println("password: " + userMap.get(userCookie.getValue()));
 	}
 
 }
