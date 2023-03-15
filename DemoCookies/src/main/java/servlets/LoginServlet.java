f8a3a41359e120523a750e2855d8d8b16eea4855
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-14 17:54:45 -0700

    Create base project to demo cookies

---
+++ DemoCookies/src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+    public LoginServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		response.getWriter().println("Login details received...");
+		response.getWriter().println("email: " + email);
+		response.getWriter().println("password: " + "*".repeat(password.length()));
+	}
+
+}
