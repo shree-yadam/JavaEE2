5f22cd7bef637d60bcfd2b9f0d4230869dbd29e2
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-15 19:52:11 -0700

    Refactot DemoCookies and DemoSession to have their respective code

--- DemoCookies/src/main/java/servlets/LoginServlet.java
+++ DemoCookies/src/main/java/servlets/LoginServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
@@ -24,25 +23,37 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		HttpSession session = request.getSession(true);
-		session.setAttribute(COOKIE_NAME, email);
+		Cookie userCookie = new Cookie(COOKIE_NAME, email);
 		userMap.put(email, password);
+		response.addCookie(userCookie);
 		response.getWriter().println(email + " logged in !!!");
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		HttpSession session = req.getSession(false);
-		if(session == null ) {
+		Cookie[] cookies = req.getCookies();
+		Cookie userCookie = null;
+		if(cookies == null ) {
 			resp.getWriter().println("No user logged in");
 			return;
 		}
-		String email = (String) session.getAttribute(COOKIE_NAME);
+		for(Cookie cookie: cookies) {
+			if(cookie.getName().equals(COOKIE_NAME)) {
+				userCookie = cookie;
+				break;
+			}
+		}
+		
+		if(null == userCookie || userCookie.getName().isEmpty()) {
+			resp.getWriter().println("No user logged in");
+			return;
+		}
 		resp.getWriter().println("Login details received...");
-		resp.getWriter().println("email: " + email);
-		resp.getWriter().println("password: " + userMap.get(email));
+		resp.getWriter().println("email: " + userCookie.getValue());
+		resp.getWriter().println("password: " + userMap.get(userCookie.getValue()));
 		resp.getWriter().println("Logging out ...");
-		session.invalidate();
+		userCookie.setMaxAge(0);
+		resp.addCookie(userCookie);
 	}
 
 }
--- DemoSession/src/main/java/servlets/LoginServlet.java
+++ DemoSession/src/main/java/servlets/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
@@ -23,37 +24,25 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		Cookie userCookie = new Cookie(COOKIE_NAME, email);
+		HttpSession session = request.getSession(true);
+		session.setAttribute(COOKIE_NAME, email);
 		userMap.put(email, password);
-		response.addCookie(userCookie);
 		response.getWriter().println(email + " logged in !!!");
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Cookie[] cookies = req.getCookies();
-		Cookie userCookie = null;
-		if(cookies == null ) {
+		HttpSession session = req.getSession(false);
+		if(session == null ) {
 			resp.getWriter().println("No user logged in");
 			return;
 		}
-		for(Cookie cookie: cookies) {
-			if(cookie.getName().equals(COOKIE_NAME)) {
-				userCookie = cookie;
-				break;
-			}
-		}
-		
-		if(null == userCookie || userCookie.getName().isEmpty()) {
-			resp.getWriter().println("No user logged in");
-			return;
-		}
+		String email = (String) session.getAttribute(COOKIE_NAME);
 		resp.getWriter().println("Login details received...");
-		resp.getWriter().println("email: " + userCookie.getValue());
-		resp.getWriter().println("password: " + userMap.get(userCookie.getValue()));
+		resp.getWriter().println("email: " + email);
+		resp.getWriter().println("password: " + userMap.get(email));
 		resp.getWriter().println("Logging out ...");
-		userCookie.setMaxAge(0);
-		resp.addCookie(userCookie);
+		session.invalidate();
 	}
 
 }
