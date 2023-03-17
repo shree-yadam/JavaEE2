0f71055bf0cb3d115fbfb1190ce64d0703012f9a
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-17 16:14:24 -0700

    Read values from DB

--- DemoDBConnection/src/main/java/servlets/Users.java
+++ DemoDBConnection/src/main/java/servlets/Users.java
@@ -3,7 +3,9 @@ package servlets;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 
 import javax.servlet.ServletException;
@@ -50,8 +52,23 @@ public class Users extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			Statement st = dbConnection.createStatement();
+
+			ResultSet result = st.executeQuery("SELECT * FROM users");
+			response.getWriter().append("<html><body><table><tbody>");
+			response.getWriter().append("<tr><td>NAME</td><td>EMAIL</td><td>PHONE NUMBER</td></tr>");
+			while(result.next()) {
+				String name = result.getString("name");
+				String phoneNumber = result.getString("phone_number");
+				String email = result.getString("email");
+				response.getWriter().append("<tr><td>" + name + "</td><td>" + email + "</td><td>" + phoneNumber + "</td></tr>");
+			}
+			response.getWriter().append("</tbody></table></body></html>");
+		} catch (SQLException e) {
+			System.out.println("SQLException");
+		}
+		
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
