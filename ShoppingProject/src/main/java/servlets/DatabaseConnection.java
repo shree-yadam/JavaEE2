01ddd654dc76317b9f59dc3f5b952aac0dca6180
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-17 19:53:12 -0700

    Create a singleton DB Conn

---
+++ ShoppingProject/src/main/java/servlets/DatabaseConnection.java
@@ -0,0 +1,61 @@
+package servlets;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class DatabaseConnection {
+	
+	private static DatabaseConnection db = null;
+	
+	Connection dbConn = null;
+	
+	private DatabaseConnection() {
+		// Initialize all the information regarding
+        // Database Connection
+		StringBuffer connectionURL = new StringBuffer(Optional.ofNullable(System.getenv("CONNECTION_URL")).orElse(""));
+        String dbDriver = Optional.ofNullable(System.getenv("DB_DRIVER")).orElse("");
+        String dbURL = Optional.ofNullable(System.getenv("DB_URL")).orElse("");
+        // Database name to access
+        String dbName = Optional.ofNullable(System.getenv("DEMO_DB_NAME")).orElse("");;
+        String dbUsername = Optional.ofNullable(System.getenv("DB_USER")).orElse("");;
+        String dbPassword = Optional.ofNullable(System.getenv("DB_PASSWORD")).orElse("");;
+        
+        connectionURL.append(dbURL);
+        connectionURL.append("/");
+        connectionURL.append(dbName);
+        
+        try {
+        	Class.forName(dbDriver);
+        	
+			dbConn = DriverManager.getConnection(connectionURL.toString(), dbUsername, dbPassword);
+		} catch (SQLException e) {
+			System.out.println("Exception getting DB connection " + e);
+		} catch (ClassNotFoundException e1) {
+			System.out.println("ClassNotFoundException " + e1);
+		}
+		
+        System.out.println("DB Connection created successfully");
+	}
+	
+	@Override
+	public void finalize() throws Throwable {
+		dbConn.close();
+		System.out.println("DB Connection closed successfully");
+	}
+
+	public static DatabaseConnection getInstance() {
+		
+		if(null == db) {
+			synchronized(DatabaseConnection.class) {
+				if(null == db) {
+					db = new DatabaseConnection();
+				}
+			}
+		}
+		
+		return db;
+	}
+
+}
--- ShoppingProject/src/main/java/servlets/Register.java
+++ ShoppingProject/src/main/java/servlets/Register.java
@@ -10,9 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/register")
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private DatabaseConnection dbConn = null;
        
     public Register() {
         super();
+        dbConn = DatabaseConnection.getInstance();
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
