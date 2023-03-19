f3e25b6c422da4293db6f6f2cc7f0f523b255d7e
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-18 22:28:46 -0700

    Implement logout

---
+++ ShoppingProject/src/main/java/servlets/Logout.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Logout() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		
+		if(session != null) {
+			session.setMaxInactiveInterval(0);
+		}
+		
+		response.sendRedirect("/ShoppingProject/login.html");
+	}
+
+}
