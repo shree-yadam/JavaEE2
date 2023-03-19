c4d27232d16d83f694544fb77262a52be08e4cae
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-18 22:19:02 -0700

    Display message for non logged in user at home page

--- ShoppingProject/src/main/java/servlets/Home.java
+++ ShoppingProject/src/main/java/servlets/Home.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Home
@@ -19,14 +20,25 @@ public class Home extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String name = "tempName";
+		HttpSession session = request.getSession(false);
+		
+
 		String contextRoute = request.getContextPath();
+		
+
 		StringBuffer sb = new StringBuffer("<html>\n\t<body>\n\t\t\n");
-		sb.append("\t\t\t<h3>Welcome " + name +"!</h3>\n");
-		sb.append("<p><a href=\""+ contextRoute + "/items\">Shop</a></p>\n");
-		sb.append("<p><a href=\""+ contextRoute + "/orders\">Order History</a></p>\n");
-		sb.append("</br></br><p><a href=\""+ contextRoute + "/logout\">Logout</a></p>\n");
-		sb.append("\t</body>\n</html>\n");
+		
+		if(session != null) {
+			String name = (String) session.getAttribute("name");
+			sb.append("\t\t\t<h3>Welcome " + name +"!</h3>\n");
+			sb.append("<p><a href=\""+ contextRoute + "/items\">Shop</a></p>\n");
+			sb.append("<p><a href=\""+ contextRoute + "/orders\">Order History</a></p>\n");
+			sb.append("</br></br><p><a href=\""+ contextRoute + "/logout\">Logout</a></p>\n");
+			sb.append("\t</body>\n</html>\n");
+		} else {
+			sb.append("\t\t\t<h3> Please <a href=\""+ contextRoute + "/login.html\">login</a> to continue</h3>\n");
+		}
+
 		response.getWriter().append(sb.toString());
 	}
 
