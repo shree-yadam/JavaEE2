7f2f9117e4deb241147e504224e5e4c8229a7c9c
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-18 22:18:37 -0700

    Add register functionality

--- ShoppingProject/src/main/java/servlets/Register.java
+++ ShoppingProject/src/main/java/servlets/Register.java
@@ -1,26 +1,58 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/register")
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	private DatabaseConnection dbConn = null;
+	private DatabaseConnection db = null;
        
     public Register() {
         super();
-        dbConn = DatabaseConnection.getInstance();
+        db = DatabaseConnection.getInstance();
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("POST REGISTER");
-		response.getWriter().append("POST REGISTER");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String phoneNumber = request.getParameter("phoneNumber");
+		String password = request.getParameter("password");
+		
+		Connection conn = db.getDbConn();
+		
+		try {
+			PreparedStatement pSt = conn.prepareStatement("INSERT into users (name, password, phone_number, email) VALUES (?, ? , ?, ?)");
+			
+			pSt.setString(1, name);
+			pSt.setString(2, password);
+			pSt.setString(3, phoneNumber);
+			pSt.setString(4, email);
+			
+			int numRows = pSt.executeUpdate();
+			
+			System.out.println(numRows + "users registered.");
+			
+			HttpSession session = request.getSession(true);
+			session.setAttribute("name", name);
+			session.setAttribute("email", email);
+			
+			response.sendRedirect("/ShoppingProject/home");
+		} catch (SQLException e) {
+			System.out.println("SQLException " + e);
+			response.sendError(500, "Error registering!");
+		}
+		
 	}
 
 }
