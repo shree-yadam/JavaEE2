76c96016649b005632ac86be376b0019c964e424
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-19 11:07:06 -0700

    Add back button and format location

--- ShoppingProject/src/main/java/servlets/Items.java
+++ ShoppingProject/src/main/java/servlets/Items.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/items")
 public class Items extends HttpServlet {
@@ -26,49 +27,63 @@ public class Items extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		Connection conn = db.getDbConn();
+		HttpSession session = request.getSession(false);
 		
 		StringBuffer sb = new StringBuffer("<html><body>");
+
+		String contextRoute = request.getContextPath();
 		
-		try {
-			Statement st = conn.createStatement();
+		if(session != null) {
+
+			Connection conn = db.getDbConn();
 			
-			ResultSet rs = st.executeQuery("SELECT * FROM items");
-			
-			if(rs.next()) {
-				sb.append("<form method=\"POST\" action=\"/ShoppingProject/orders\">");
-				sb.append("<table style=\"border: 1px solid gray \"><tbody>");
-				sb.append("<tr  style=\"border: 1px solid gray \">");
-				sb.append("<td  style=\"border: 1px solid gray \"></td>");
-				sb.append("<td  style=\"border: 1px solid gray; color: blue \"><h2>ITEM</h2></td>");
-				sb.append("<td  style=\"border: 1px solid gray; color: blue \"><h2>Price</h2></td>");
-				sb.append("<td  style=\"border: 1px solid gray; color: blue \"><h2>Quantity</h2></td>");
-				sb.append("</tr>");
-			
-				do {
-					int id = rs.getInt("id");
-					String name = rs.getString("name");
-					Double price = ((double) rs.getInt("price"))/ 100;
-					String description = rs.getString("description");
-					String imgURL = rs.getString("img_url");
-					int quantity = rs.getInt("quantity");
-					
-					sb.append("<tr style=\"margin: 20px 0;border: 1px solid gray \">");
-					sb.append("<td  style=\"border: 1px solid gray \"><img src=\""+ imgURL + "\" alt=\"item\" width=70px ></td>");
-					sb.append("<td  style=\"border: 1px solid gray \"><h3 style=\"margin: 5px 0; color: blue \">" + name + "</h3><pstyle=\"margin: 5px 0 \">" + description + "</p></td>");
-					sb.append("<td  style=\"border: 1px solid gray \"><h5>" + price + "</h5></td>");
-					sb.append("<td  style=\"border: 1px solid gray \"><input type=\"number\" min=\"1\" max=\"" + quantity + "\" id=\"" + id + "\" name=\"" + id + "\"></td>");
-					sb.append("<tr>");
+		
+		
+			try {
+				Statement st = conn.createStatement();
+				
+				ResultSet rs = st.executeQuery("SELECT * FROM items");
+				
+				if(rs.next()) {
+					sb.append("<form method=\"POST\" action=\"/ShoppingProject/orders\">");
+					sb.append("<table style=\"border: 1px solid gray \"><tbody>");
+					sb.append("<tr  style=\"border: 1px solid gray \">");
+					sb.append("<td  style=\"border: 1px solid gray \"></td>");
+					sb.append("<td  style=\"border: 1px solid gray; color: blue \"><h2>ITEM</h2></td>");
+					sb.append("<td  style=\"border: 1px solid gray; color: blue \"><h2>Price</h2></td>");
+					sb.append("<td  style=\"border: 1px solid gray; color: blue \"><h2>Quantity</h2></td>");
+					sb.append("</tr>");
+				
+					do {
+						int id = rs.getInt("id");
+						String name = rs.getString("name");
+						Double price = ((double) rs.getInt("price"))/ 100;
+						String description = rs.getString("description");
+						String imgURL = rs.getString("img_url");
+						int quantity = rs.getInt("quantity");
 						
-				}while(rs.next());
-				
-				sb.append("</tbody></table>");
-				sb.append("<button type=\"submit\">Purchase</button>");
-				
-				sb.append("</form>");
+						sb.append("<tr style=\"margin: 20px 0;border: 1px solid gray \">");
+						sb.append("<td  style=\"border: 1px solid gray \"><img src=\""+ imgURL + "\" alt=\"item\" width=70px ></td>");
+						sb.append("<td  style=\"border: 1px solid gray \"><h3 style=\"margin: 5px 0; color: blue \">" + name + "</h3><pstyle=\"margin: 5px 0 \">" + description + "</p></td>");
+						sb.append("<td  style=\"border: 1px solid gray \"><h5>" + price + "</h5></td>");
+						sb.append("<td  style=\"border: 1px solid gray \"><input type=\"number\" min=\"1\" max=\"" + quantity + "\" id=\"" + id + "\" name=\"" + id + "\"></td>");
+						sb.append("<tr>");
+							
+					}while(rs.next());
+					sb.append("<tr>");
+					sb.append("<td><button type=\"button\" onclick=\"window.location.href='" + contextRoute + "/home';\">Back</button></td>");
+					sb.append("<td></td><td></td>");
+					sb.append("<td><button type=\"submit\">Purchase</button></td>");
+					sb.append("</tbody></table>");
+					
+					
+					sb.append("</form>");
+				}
+			} catch (SQLException e) {
+				System.out.println("SQLException " + e);
 			}
-		} catch (SQLException e) {
-			System.out.println("SQLException " + e);
+		} else {
+			sb.append("<h3> Please <a href=\""+ contextRoute + "/login.html\">login</a> to continue</h3>");
 		}
 		
 		sb.append("</body></html>");
