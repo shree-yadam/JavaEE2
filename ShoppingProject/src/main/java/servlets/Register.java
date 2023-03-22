3688fe9ed8f8c44bb1da62a336859ed811962c64
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-21 21:15:06 -0700

    Use the register user method to register a new user

--- ShoppingProject/src/main/java/servlets/Register.java
+++ ShoppingProject/src/main/java/servlets/Register.java
@@ -1,9 +1,6 @@
 package servlets;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,47 +9,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.User;
+
 @WebServlet("/register")
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	private DatabaseConnection db = null;
        
-    public Register() {
-        super();
-        db = DatabaseConnection.getInstance();
-    }
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
 		String phoneNumber = request.getParameter("phoneNumber");
 		String password = request.getParameter("password");
 		
-		Connection conn = db.getDbConn();
-		
-		try {
-			PreparedStatement pSt = conn.prepareStatement("INSERT into users (name, password, phone_number, email) VALUES (?, ? , ?, ?)");
+		if(DatabaseConnection.getInstance().getUserForEmail(email) != null) {
+			System.out.println("User already exists");
 			
-			pSt.setString(1, name);
-			pSt.setString(2, password);
-			pSt.setString(3, phoneNumber);
-			pSt.setString(4, email);
-			
-			int numRows = pSt.executeUpdate();
-			
-			System.out.println(numRows + "users registered.");
-			
-			HttpSession session = request.getSession(true);
-			session.setAttribute("name", name);
-			session.setAttribute("email", email);
-			
-			pSt.close();
-			response.sendRedirect("/ShoppingProject/home");
-		} catch (SQLException e) {
-			System.out.println("SQLException " + e);
-			response.sendError(500, "Error registering!");
+			// TODO: User already exists error page
 		}
+		
+		DatabaseConnection.getInstance().registerUser(new User(name, email, phoneNumber, password));
+		
+		HttpSession session = request.getSession(true);
+		session.setAttribute("name", name);
+		session.setAttribute("email", email);
+		response.sendRedirect("/ShoppingProject/home");
 		
 	}
 
