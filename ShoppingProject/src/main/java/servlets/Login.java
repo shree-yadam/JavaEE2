f40bf7db0d9c5b4fa2272ed2049e46d7c1537893
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-21 21:18:52 -0700

    Use getUserByEmail method in login

--- ShoppingProject/src/main/java/servlets/Login.java
+++ ShoppingProject/src/main/java/servlets/Login.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.User;
+
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -31,7 +33,27 @@ public class Login extends HttpServlet {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		
-		PreparedStatement pSt;
+		User user = DatabaseConnection.getInstance().getUserForEmail(email);
+		
+		if(user != null && password.equals(user.getPassword())) {
+			HttpSession session = request.getSession(true);
+			session.setAttribute("name", user.getName());
+			session.setAttribute("email", email);
+			
+			response.sendRedirect("/ShoppingProject/home");
+		} else {
+			String contextRoute = request.getContextPath();
+			
+			StringBuffer sb = new StringBuffer("<html><body>");
+			sb.append("<h2> Invalid email or password</h2>");
+			sb.append("<h3> Please <a href=\""+ contextRoute + "/login.html\">login</a> to continue</h3>");
+			sb.append("</body></html>");
+			
+			response.getWriter().append(sb.toString());
+		}
+		
+		
+		/*PreparedStatement pSt;
 		try {
 			pSt = conn.prepareStatement("SELECT * FROM users WHERE email = ?");
 
@@ -64,9 +86,7 @@ public class Login extends HttpServlet {
 			pSt.close();
 		} catch (SQLException e) {
 			System.out.println("SQLException " + e);
-		}
-		
-		
+		}*/	
 	}
 
 }
